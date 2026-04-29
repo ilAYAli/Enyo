692a13c537f0f61a2a58f5c171de151b076d34ce
Petter Wahlman <pwahlman@cisco.com>
2026-04-29 15:36:39 +0200

    remove unused stockfish-compatible zobrist hash
    generate_stockfish_hash, its stockfish_psq_ oracle table, the
    stockfish-layout piece/square mappers in the zbrs ctor, the
    has_legal_ep / ep_file helpers, and the `sfhash` UCI debug command
    were all only used for a one-off compatibility check during the
    hash correctness investigation. With generate_hash now verified
    against the incremental path via the hash.* tests, the oracle has
    no consumers.

--- zobrist.cpp
+++ zobrist.cpp
@@ -5,27 +5,6 @@ using namespace enyo;
 
 namespace zobrist {
 
-constexpr int ep_file(square_t sq)
-{
-    return 7 - (sq % 8);
-}
-
-bool has_legal_ep(const Board & b)
-{
-    const auto ep = b.gamestate.enpassant_square;
-    if (!ep)
-        return false;
-
-    const auto side = b.side;
-    const auto pawns = b.pt_bb[side][pawn];
-    const auto ep_mask = 1ULL << ep;
-    const auto attackers = side == white
-        ? ((ep_mask >> 7) & ~file_h) | ((ep_mask >> 9) & ~file_a)
-        : ((ep_mask << 7) & ~file_a) | ((ep_mask << 9) & ~file_h);
-
-    return static_cast<bool>(pawns & attackers);
-}
-
 uint64_t generate_hash(Board const & b)
 {
     uint64_t zkey = 0;
@@ -53,36 +32,6 @@ uint64_t generate_hash(Board const & b)
         zkey ^= b.zbrs.side_;
 
     return zkey;
-}
-
-uint64_t generate_stockfish_hash(Board const & b)
-{
-    uint64_t key = 0;
-
-    for (int square = 0; square < 64; square++) {
-        const auto pt = b.pt_mb[square];
-        if (pt == no_piece_type)
-            continue;
-
-        const auto sq_mask = 1ULL << square;
-        const auto color = (b.color_bb[white] & sq_mask) ? white : black;
-        const int sf_piece = color == white
-            ? (pt == pawn ? 1 : pt == knight ? 2 : pt == bishop ? 3 : pt == rook ? 4 : pt == queen ? 5 : 6)
-            : (pt == pawn ? 9 : pt == knight ? 10 : pt == bishop ? 11 : pt == rook ? 12 : pt == queen ? 13 : 14);
-        const int sf_square = 7 - (square % 8) + 8 * (square / 8);
-        key ^= b.zbrs.stockfish_psq_[sf_piece][sf_square];
-    }
-
-    if (has_legal_ep(b)) {
-        const auto file = ep_file(b.gamestate.enpassant_square);
-        key ^= b.zbrs.enpassant_[file];
-    }
-
-    if (!b.gamestate.white_to_move)
-        key ^= b.zbrs.side_;
-
-    key ^= b.zbrs.castling_[b.gamestate.castling_rights];
-    return key;
 }
 
 } // zobrist
