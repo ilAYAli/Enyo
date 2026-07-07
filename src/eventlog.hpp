05d55528d8cc92e1a5bf214ba06518679f9724e1
Petter Wahlman <petter@wahlman.no>
2026-07-07 07:15:38 +0200

    fix: truncate game log on startup

--- src/eventlog.hpp
+++ src/eventlog.hpp
@@ -1,15 +1,14 @@
 #pragma once
 
-#include <fstream>
-#include <unistd.h>
-#include <fmt/format.h>
-#include "config.hpp"
-#include <thread>
-#include <sstream>
-#include <mutex>
 #include <atomic>
+#include <fstream>
+#include <mutex>
 #include <string_view>
 
+#include <fmt/format.h>
+#include <unistd.h>
+
+#include "config.hpp"
 
 namespace eventlog {
 
@@ -28,80 +27,29 @@ enum class Log {
 
 
 constexpr inline auto defaultLogLevel = Log::uci;
-constexpr inline auto logfilesToKeep = 20;
 
-namespace fs = std::filesystem;
-
-inline void remove_old_logfiles(const std::string& baseFilename, std::size_t maxFiles) {
-    fs::path basePath(baseFilename);
-    std::string logFilePattern = basePath.stem().string() + "_";
-    fs::path logDir = basePath.parent_path();
-
-    if (!fs::exists(logDir))
-        return;
-
-    struct LogFile {
-        fs::path path;
-        fs::file_time_type timestamp;
-    };
-
-    std::vector<LogFile> logFiles;
-
-    for (const auto& entry : fs::directory_iterator(logDir)) {
-        if (entry.is_regular_file()) {
-            const auto& path = entry.path();
-            if (path.filename().string().find(logFilePattern) == 0) {
-                std::error_code ec;
-                const auto timestamp = fs::last_write_time(path, ec);
-                if (!ec)
-                    logFiles.push_back({path, timestamp});
-            }
-        }
-    }
-
-    std::sort(logFiles.begin(), logFiles.end(), [](const LogFile& a, const LogFile& b) {
-        if (a.timestamp != b.timestamp)
-            return a.timestamp < b.timestamp;
-        return a.path.filename() < b.path.filename();
-    });
-
-    while (logFiles.size() > maxFiles) {
-        std::error_code ec;
-        fs::remove(logFiles.front().path, ec);
-        logFiles.erase(logFiles.begin());
-    }
-}
-
-
-inline std::string getLogFilename(const std::string& baseFilename) {
-    std::ostringstream oss;
-    oss << baseFilename.substr(0, baseFilename.find_last_of('.'))
-        << "_" << getpid()
-        << "_" << std::this_thread::get_id()
-        << baseFilename.substr(baseFilename.find_last_of('.'));
-    return oss.str();
-}
-
-inline std::string getDefaultLogFilename()
-{
-    return getLogFilename(enyo::cfgmgr.logfile);
-}
-
-inline std::string logFilename = getDefaultLogFilename();
+inline std::string logFilename;
 inline std::ofstream logFile;
 inline std::mutex logMutex;
 
-inline void reopen_logfile(const std::string& filename, bool exact_path = false)
+inline void reopen_logfile(const std::string& filename, bool truncate = false)
 {
     std::lock_guard lock(logMutex);
+
+    // settings.json is replayed through UCI after startup. Do not reopen the
+    // same file then: doing so would either append forever or erase the startup
+    // banner that was just written.
+    if (logFile.is_open() && logFilename == filename)
+        return;
+
     logFile.close();
-    logFilename = exact_path ? filename : getLogFilename(filename);
-    logFile.open(logFilename, std::ios::app);
+    logFilename = filename;
+    logFile.open(logFilename, truncate ? std::ios::trunc : std::ios::app);
 }
 
-inline void init() {
-    remove_old_logfiles(enyo::cfgmgr.logfile, logfilesToKeep);
-    reopen_logfile(enyo::cfgmgr.logfile);
+inline void init()
+{
+    reopen_logfile(enyo::cfgmgr.logfile, true);
 }
 
 template <Log level = Log::info, typename... T>
